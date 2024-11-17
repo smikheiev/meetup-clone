4a90fa6f5413b967197c0ccbf84429e94514dcf9
Serhii Mikheiev <mikh.s.o@gmail.com>
2024-11-17 22:31:38 +0200

    show data in event details placeholder

--- client-app/features/events/EventDetails.tsx
+++ client-app/features/events/EventDetails.tsx
@@ -1,4 +1,6 @@
-import { Text } from 'react-native'
+import Feather from '@expo/vector-icons/Feather'
+import { format } from 'date-fns'
+import { Image, Text, View } from 'react-native'
 
 import type { Event } from './types'
 
@@ -7,5 +9,15 @@ type Props = {
 }
 
 export default function EventDetails({ event }: Props) {
-  return <Text>{event.title}</Text>
+  return (
+    <View>
+      <Image className="aspect-video w-2/5" source={{ uri: event.image }} />
+      <Text>{event.title}</Text>
+      <Feather name="calendar" size={20} color="gray" />
+      <Text>{format(event.startDatetime, 'EEEE, d LLLL y')}</Text>
+      <Text>{format(event.startDatetime, 'HH:mm')}</Text>
+      <Feather name="map-pin" size={20} color="gray" />
+      <Text>{event.location}</Text>
+    </View>
+  )
 }
